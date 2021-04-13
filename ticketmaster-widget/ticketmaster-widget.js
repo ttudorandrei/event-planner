02f404f48d43efe7ffe25d72e5cf432de0e69f49
Tudor Tocan <tudor.tocan@gmail.com>
2021-04-13 21:14:09 +0100

    add widget html and js

---
+++ ticketmaster-widget/ticketmaster-widget.js
@@ -0,0 +1,22 @@
+// JS for WIDGET
+const button = document.getElementById("button");
+const cityInput = $("#city-name-input");
+const country = $("#country");
+
+// this function will dynamically create the widget and append it along with its script
+const createWidget = () => {
+  //this is the code for the widget
+  const widget = `<div w-type="event-discovery" w-tmapikey="0GNTLEb6ffjAj82DU3Zip5wqIzQqqi1f" w-googleapikey="YOUR_GOOGLE_API_KEY" w-keyword="" w-theme="simple" w-colorscheme="light" w-width="400" w-height="600" w-size="25" w-border="0" w-borderradius="20" w-postalcode="" w-radius="25" w-city="${cityInput.val()}" w-period="week" w-layout="vertical" w-attractionid="" w-promoterid="" w-venueid="" w-affiliateid="" w-segmentid="" w-proportion="custom" w-titlelink="off" w-sorting="groupByName" w-id="id_9npyeo7" w-countrycode="" w-source="" w-branding="Ticketmaster" w-latlong=""></div>`;
+
+  //this is the code for the widget script. It needs to be dynamically created and appended along with the widget, otherwise it won`t work
+  const widgetScript = `<script src="https://ticketmaster-api-staging.github.io/products-and-docs/widgets/event-discovery/1.0.0/lib/main-widget.js"></script>`;
+
+  //this code will make sure that the container is empty before appending any widgets and then append. This will ultimately allow for multiple searches without the widget being generated multiple times
+  $("#widget-container").empty();
+  $("#widget-container").append(widget);
+  $("#widget-script").append(widgetScript);
+};
+
+button.onclick = function () {
+  createWidget();
+};
