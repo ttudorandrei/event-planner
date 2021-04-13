539effc251a2137afda48002e42f032d4ebec46d
Misha <73886737+misha244@users.noreply.github.com>
2021-04-13 21:46:11 +0100

    header

--- assets/js/carousel.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  $(".slider").slider({
-    indicators: false,
-    interval: 3000,
-  });
-  $("select").formSelect();
-});
--- assets/js/image-carousel.js
+++ assets/js/image-carousel.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   $(".slider").slider({
+    height: 700,
     indicators: false,
     interval: 3000,
   });
