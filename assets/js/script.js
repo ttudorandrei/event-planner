67323cb4fb6aa0117dee129b92a185c997e11334
Tudor Tocan <tudor.tocan@gmail.com>
2021-04-15 20:30:31 +0100

    refactor script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,41 @@
-// activates carousel
-$(document).ready(function () {
+const getFormData = () => {
+  const inputValue = $("#form-input").val();
+  const countryValue = $("#country-input").val();
+
+  const wantsRestaurants = $("#restaurant").is(":checked");
+  const wantsArts = $("#arts-entertainment").is(":checked");
+  const wantsOutdoors = $("#outdoor-recreation").is(":checked");
+
+  const formData = {
+    inputValue,
+    countryValue,
+    wantsRestaurants,
+    wantsArts,
+    wantsOutdoors,
+  };
+
+  return formData;
+};
+
+const onSubmit = (event) => {
+  event.preventDefault();
+  const formData = getFormData();
+  console.log(formData);
+};
+
+const onReady = () => {
   $(".slider").slider({
     height: 900,
     indicators: false,
     interval: 3000,
   });
 
-  const getFormData = () => {
-    const inputValue = $("#form-input").val();
-    const countryValue = $("#country-input").val();
-
-    const wantsRestaurants = $("#restaurant").is(":checked");
-    const wantsArts = $("#arts-entertainment").is(":checked");
-    const wantsOutdoors = $("#outdoor-recreation").is(":checked");
-
-    const formData = {
-      inputValue,
-      countryValue,
-      wantsRestaurants,
-      wantsArts,
-      wantsOutdoors,
-    };
-
-    return formData;
-  };
-
-  const onSubmit = (event) => {
-    event.preventDefault();
-    const formData = getFormData();
-    console.log(formData);
-  };
-
   // target form and add submit event listener
   $("#form").submit(onSubmit);
 
   // activates dropdown on form
   $("select").formSelect();
-});
+};
+
+// activates carousel
+$(document).ready(onReady);
