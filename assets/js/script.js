c3c8fb672ac45f92c3a455be709a28b23a0db836
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-04-15 19:04:10 +0100

    targeted form and added on submit function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,22 @@
+// activates carousel
 $(document).ready(function () {
   $(".slider").slider({
     height: 900,
     indicators: false,
     interval: 3000,
   });
+
+  const getFormData = (event) => {
+    console.log(event);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    getFormData(event);
+  };
+
+  $("#form").submit(onSubmit);
+
+  // activates dropdown on form
   $("select").formSelect();
 });
